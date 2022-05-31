c38886e2f216fd7b8dd5f7c382fc4da82770499a
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2022-05-31 17:18:50 +1000

    Added about page

---
+++ pages/about.tsx
@@ -0,0 +1,70 @@
+import { ReactElement } from 'react'
+import Link from 'next/link'
+import styled from 'styled-components'
+
+import Layout from '../components/layout'
+
+const StyledAbout = styled.div`
+  position: relative;
+`
+
+const StyledContainer = styled.div`
+  padding: 0 20px;
+  max-width: 75em;
+  margin-left: auto;
+  margin-right: auto;
+
+  @media screen and (min-width: 30em) {
+    max-width: 100%;
+  }
+
+  @media screen and (min-width: 48em) {
+    max-width: 100%;
+  }
+
+  @media screen and (min-width: 60.625em) {
+    max-width: 60.625em;
+  }
+
+  @media screen and (min-width: 75em) {
+    max-width: 60.625em;
+  }
+`
+
+const StyledContent = styled.div`
+  position: relative;
+  height: 100vh;
+  width: 100%;
+`
+
+const StyledTextContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const StyledLink = styled.a`
+  text-decoration: none;
+  cursor: pointer;
+`
+
+export default function About() {
+  return (
+    <StyledAbout>
+      <StyledContainer>
+        <StyledContent>
+          <StyledTextContainer>
+            <Link href="/">
+              <StyledLink>Go back home!</StyledLink>
+            </Link>
+          </StyledTextContainer>
+        </StyledContent>
+      </StyledContainer>
+    </StyledAbout>
+  )
+}
+
+About.getLayout = function getLayout(page: ReactElement) {
+  return <Layout>{page}</Layout>
+}
