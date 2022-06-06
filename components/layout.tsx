094a7934314099475ea1af3f38ef14a3d8671784
Muhammad Wildan Aziz <wildanazzwa@gmail.com>
2022-06-07 00:17:38 +1000

    Refactored particles component

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import dynamic from 'next/dynamic'
 import styled from 'styled-components'
 import { AnimatePresence } from 'framer-motion'
 
-const Particles = dynamic(() => import('react-tsparticles'))
+const Background = dynamic(() => import('./background'))
 const Navbar = dynamic(() => import('./navbar'))
 
 const StyledLayout = styled.div`
@@ -17,64 +17,7 @@ export default function Layout({
 }) {
   return (
     <StyledLayout>
-      <Particles
-        options={{
-          background: {
-            image: '-webkit-radial-gradient(bottom, #10181f, #00020d)',
-          },
-          fullScreen: {
-            zIndex: -1,
-          },
-          particles: {
-            color: {
-              value: '#ffcb74',
-            },
-            move: {
-              attract: {
-                rotate: {
-                  x: 600,
-                  y: 600,
-                },
-              },
-              enable: true,
-              outModes: { default: 'out' },
-              random: true,
-              speed: 1,
-            },
-            number: {
-              density: {
-                enable: true,
-              },
-              value: 160,
-            },
-            opacity: {
-              random: true,
-              value: {
-                min: 0,
-                max: 1,
-              },
-              animation: {
-                enable: true,
-                speed: 1,
-                minimumValue: 0,
-              },
-            },
-            size: {
-              random: true,
-              value: {
-                min: 1,
-                max: 3,
-              },
-              animation: {
-                enable: true,
-                speed: 4,
-                minimumValue: 0,
-                startValue: 'min',
-              },
-            },
-          },
-        }}
-      />
+      <Background />
       <AnimatePresence
         initial={false}
         exitBeforeEnter
